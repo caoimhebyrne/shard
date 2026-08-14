54448513536d3f9a9339c2e07cae97cc2e7d0fb4
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2026-08-15 00:11:52 +0100

    chore: add rustfmt config

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use crate::config::v1::ShardConfig;
 mod config;
 
 fn main() {
-    let file_path = env::args()
-        .nth(1)
-        .expect("expected a file path to be passed as an arg");
+    let file_path = env::args().nth(1).expect("expected a file path to be passed as an arg");
     let contents = fs::read_to_string(file_path).unwrap();
     let config = ShardConfig::from_str(&contents).unwrap();
     println!("Config: {:#?}", config);
