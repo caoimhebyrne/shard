545ea7cfd8f6adc39dd28275f0262e638172cacd
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2026-08-15 11:32:45 +0100

    chore: configure clippy and add a ci workflow

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,5 @@ fn main() {
     let file_path = env::args().nth(1).expect("expected a file path to be passed as an arg");
     let contents = fs::read_to_string(file_path).unwrap();
     let config = ShardConfig::from_str(&contents).unwrap();
-    println!("Config: {:#?}", config);
+    println!("Config: {config:#?}");
 }
