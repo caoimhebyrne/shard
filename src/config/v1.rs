9b52a72dc5e718a0ac5efef0d7060bb95f078e30
Caoimhe <71222289+caoimhebyrne@users.noreply.github.com>
2026-08-15 14:27:19 +0100

    feat(instance): implement instance resolving (#1)

--- src/config/v1.rs
+++ src/config/v1.rs
@@ -7,32 +7,49 @@ use crate::{config::BaseShardConfig, error::yaml_serde::YamlSerdeError};
 
 /// A configuration for the shard system.
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct ShardConfig {
     /// The version that this [`ShardConfig`] was defined at.
     pub version: u8,
 
     /// The templates within this [`ShardConfig`].
+    #[serde(default)]
     pub templates: BTreeMap<String, ShardTemplate>,
 
-    /// The matrix defining the output instances.
-    pub matrix: Vec<ShardElement>,
+    /// The matrixes defining the output instances.
+    #[serde(default)]
+    pub matrixes: Vec<ShardMatrix>,
 }
 
 /// A template to be used to generate instances within a matrix.
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
 pub struct ShardTemplate {
     /// The name that should be applied to the instance once created.
     pub name: String,
 
     /// The string describing the loader.
-    pub loader: String,
+    pub loader: Option<String>,
+
+    /// The mods that this template requires, a map from key to list of mod descriptors.
+    #[serde(default)]
+    pub mods: BTreeMap<String, Vec<String>>,
+
+    /// The JVM arguments that should be applied to generated instances.
+    #[serde(default)]
+    pub jvm_args: Vec<String>,
 }
 
 /// An element of the shard matrix, which defines the instances to generate.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ShardElement {
+#[serde(deny_unknown_fields)]
+pub struct ShardMatrix {
     /// The name of the template that this element uses.
     pub uses: String,
+
+    /// The inputs passed to the template to be used as an expansion.
+    #[serde(default)]
+    pub with: BTreeMap<String, Vec<String>>,
 }
 
 impl ShardConfig {
---
+++ src/instance/mod.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+use std::collections::BTreeMap;
+
+pub mod resolver;
+
+/// A resolved instance built from a matrix within a [`ShardConfig`].
+#[derive(Debug)]
+pub struct ResolvedInstance {
+    /// The literal name of the instance (i.e. with all templates expanded).
+    pub name: String,
+
+    /// The loader that the instance should use.
+    pub loader: Option<String>,
+
+    /// A map of mod provider to mod descriptor.
+    pub mods: BTreeMap<String, Vec<String>>,
+
+    /// Information about how the instance was resolved (template, inputs, ...)
+    pub source: InstanceSource,
+}
+
+/// Information about how an instance was resolved.
+#[derive(Debug)]
+pub struct InstanceSource {
+    /// The name of the template that the instance was derived from.
+    pub template: String,
+
+    /// The inputs used to generate the instance.
+    pub inputs: BTreeMap<String, String>,
+}
